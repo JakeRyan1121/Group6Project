acca9af89d3158e5d06109f040ea426f947aef4d
JakeRyan1121 <jakeryanstephenson@gmail.com>
2023-10-28 15:09:23 -0400

    Added Javadoc and comments

--- Project/src/Car.java
+++ Project/src/Car.java
@@ -1,6 +1,9 @@
-
+/**
+ * Represents a Car with attributes such as ID, mileage, MPG, and cost.
+ * Also provides functionalities like comparison and selling.
+ */
 public class Car {
-	private String id = "";
+	private String id;
 	private int mileage;
 	private int mpg;
 	private double cost;
@@ -8,113 +11,122 @@ public class Car {
 	private boolean sold;
 	private double priceSold;
 	private double profit;
-	// Constructors
-	Car() {
+
+	/**
+	 * Default constructor.
+	 */
+	public Car() {
+		// Initializes to default values.
 	}
-	
-	Car(String id, int mileage, int mpg, double cost, double salesprice) {
+
+	/**
+	 * Overloaded constructor for creating a new car.
+	 * @param id Car ID
+	 * @param mileage Mileage of the car
+	 * @param mpg Miles per gallon
+	 * @param cost Cost of the car
+	 * @param salesPrice Selling price
+	 */
+	public Car(String id, int mileage, int mpg, double cost, double salesPrice) {
 		this.id = id;
 		this.mileage = mileage;
 		this.mpg = mpg;
 		this.cost = cost;
 		this.salesPrice = salesPrice;
-		
 	}
-	// Accessors
-	public java.lang.String getID() {
-		return this.id;
-	}
-	
-	public int getMileage() {
-		return this.mileage;
-	}
-	
-	public int getMpg() {
-		return this.mpg;
-	}
-	
-	public double getCost() {
-		return this.cost;
-	}
-	
-	public boolean isSold() {
-		return this.sold;
-	}
-	
-	public double getPriceSold() {
-		return this.priceSold;
-	}
-	
-	public double getProfit() {
-		return this.profit;
-	}
-	
+
+	// Accessors with minor JavaDoc comments.
+	/** @return Car ID */
+	public String getID() { return this.id; }
+
+	/** @return Mileage */
+	public int getMileage() { return this.mileage; }
+
+	/** @return MPG */
+	public int getMpg() { return this.mpg; }
+
+	/** @return Cost */
+	public double getCost() { return this.cost; }
+
+	/** @return Sold status */
+	public boolean isSold() { return this.sold; }
+
+	/** @return Sold price */
+	public double getPriceSold() { return this.priceSold; }
+
+	/** @return Profit */
+	public double getProfit() { return this.profit; }
+
+	/**
+	 * @return Human-readable string representation of the Car object.
+	 */
+	@Override
 	public String toString() {
-		return id + ", Mileage: " + mileage + ", MPG: " + mpg + ", Sold: " + sold + 
+		return id + ", Mileage: " + mileage + ", MPG: " + mpg + ", Sold: " + sold +
 				", Cost: " + cost + ", Selling Price: " + salesPrice + ", Sold For: " +
 				priceSold + ", Profit: " + profit;
 	}
-	
-	public int compareMPG(Car b) {
-		if (this.mpg > b.mpg) {
-			return 1;
-		}
-		else if (this.mpg < b.mpg) {
-			return -1;
-		}
-		
-		else {
-			return 0;
-		}
+
+	// Comparison methods
+	/**
+	 * Compare cars based on MPG.
+	 * @param otherCar Car to compare against
+	 * @return int representing comparison result
+	 */
+	public int compareMPG(Car otherCar) {
+		return Integer.compare(this.mpg, otherCar.mpg);
 	}
-	
-	public int compareMiles(Car b) {
-		if (this.mileage > b.mileage) {
-			return 1;
-		}
-		else if (this.mileage < b.mileage) {
-			return -1;
-		}
-		
-		else {
-			return 0;
-		}
-		
+
+	/**
+	 * Compare cars based on mileage.
+	 * @param otherCar Car to compare against
+	 * @return int representing comparison result
+	 */
+	public int compareMiles(Car otherCar) {
+		return Integer.compare(this.mileage, otherCar.mileage);
 	}
-	
-	public int comparePrice(Car b) {
-		if (this.salesPrice > b.salesPrice) {
-			return 1;
-		}
-		else if (this.salesPrice < b.salesPrice) {
-			return -1;
-		}
-		
-		else {
-			return 0;
-		}
+
+	/**
+	 * Compare cars based on selling price.
+	 * @param otherCar Car to compare against
+	 * @return int representing comparison result
+	 */
+	public int comparePrice(Car otherCar) {
+		return Double.compare(this.salesPrice, otherCar.salesPrice);
 	}
+
 	// Mutators
-	public void setID(String id) {
-		this.id = id;
-	}
-	
-	public void setMileage(int mileage) {
-		this.mileage = mileage;
-	}
-	
-	public void setMpg(int mpg) {
-		this.mpg = mpg;
-	}
-	
-	public void setCost(double cost) {
-		this.cost = cost;
-	}
-	
+	/**
+	 * Sets the ID of the car.
+	 * @param id New ID
+	 */
+	public void setID(String id) { this.id = id; }
+
+	/**
+	 * Sets the mileage of the car.
+	 * @param mileage New mileage
+	 */
+	public void setMileage(int mileage) { this.mileage = mileage; }
+
+	/**
+	 * Sets the MPG of the car.
+	 * @param mpg New MPG
+	 */
+	public void setMpg(int mpg) { this.mpg = mpg; }
+
+	/**
+	 * Sets the cost of the car.
+	 * @param cost New cost
+	 */
+	public void setCost(double cost) { this.cost = cost; }
+
+	/**
+	 * Sells the car.
+	 * @param priceSold Price for which the car was sold
+	 */
 	public void sellCar(double priceSold) {
 		this.sold = true;
 		this.priceSold = priceSold;
-		this.profit = this.priceSold - this.salesPrice;
-		
+		this.profit = this.priceSold - this.cost;
 	}
 }
