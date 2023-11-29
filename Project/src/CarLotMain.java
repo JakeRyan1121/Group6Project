9c3a179ffc62f6e2df049c9bb390be7153ec60ca
katran1 <149259177+katran1@users.noreply.github.com>
2023-11-29 16:22:39 -0500

    Update CarLotMain.java

--- Project/src/CarLotMain.java
+++ Project/src/CarLotMain.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
  * Main class that interacts with the CarLot and manages the car inventory.
  */
 public class CarLotMain {
+	
 
     /**
      * The main method that runs the CarLot management system.
@@ -31,71 +32,72 @@ public class CarLotMain {
             System.out.println("[9] Save current Data to Disk");
             System.out.println("[10] Load Data from Disk");
 
-            System.out.print("Enter a number from 0 to 8: ");
+            System.out.print("Enter a number from 0 to 10: ");
             option = input.nextInt();
 
-            if (option == 0) {
-                System.out.println("Exiting. Goodbye");
-            } else if (option == 1) {
-                // Adds a car to inventory
-                Car car = createCar(input);
-                carLot.addCar(car);
-                System.out.println("Car added to the inventory.");
-            } else if (option == 2) {
-                // Sells a car from inventory
-                System.out.print("Enter the car identifier to sell: ");
-                String identifier = input.next();
-                System.out.print("Enter how much it was sold for: ");
-                double priceSold = input.nextDouble();
-                try {
-                    carLot.sellCar(identifier, priceSold);
-                    System.out.println("Car sold successfully.");
-                } catch (IllegalArgumentException e) {
-                    System.out.println("Error: " + e.getMessage());
-                }
-            } else if (option == 3) {
-                // List inventory by order of acquisition
-                displayCars(carLot.getCarsInOrderOfEntry());
-            } else if (option == 4) {
-                // List inventory from Best MPG to Worst MPG
-                displayCars(carLot.getCarsSortedByMPG());
-            } else if (option == 5) {
-                // Display car with Best MPG
-                Car bestMpgCar = carLot.getCarWithBestMPG();
-                if (bestMpgCar != null) {
-                    System.out.println("Car with the best MPG:\n" + bestMpgCar);
+            try {
+                if (option == 0) {
+                    System.out.println("Exiting. Goodbye");
+                } else if (option == 1) {
+                    // Adds a car to inventory
+                    Car car = createCar(input);
+                    carLot.addCar(car);
+                    System.out.println("Car added to the inventory.");
+                } else if (option == 2) {
+                    // Sells a car from inventory
+                    System.out.print("Enter the car identifier to sell: ");
+                    String identifier = input.next();
+                    System.out.print("How much is it sold for?: ");
+                    double priceSold = input.nextDouble();
+                    try {
+                        carLot.sellCar(identifier, priceSold);
+                        System.out.println("Car sold successfully.");
+                    } catch (IllegalArgumentException e) {
+                        System.out.println("Error: " + e.getMessage());
+                    }
+                } else if (option == 3) {
+                    // List inventory by order of acquisition
+                    displayCars(carLot.getCarsInOrderOfEntry());
+                } else if (option == 4) {
+                    // List inventory from Best MPG to Worst MPG
+                    displayCars(carLot.getCarsSortedByMPG());
+                } else if (option == 5) {
+                    // Display car with Best MPG
+                    Car bestMpgCar = carLot.getCarWithBestMPG();
+                    if (bestMpgCar != null) {
+                        System.out.println("Car with the best MPG:\n" + bestMpgCar);
+                    } else {
+                        System.out.println("No cars in the inventory.");
+                    }
+                } else if (option == 6) {
+                    // Display car with the highest mileage
+                    Car highestMileageCar = carLot.getCarWithHighestMileage();
+                    if (highestMileageCar != null) {
+                        System.out.println("Car with the highest mileage:\n" + highestMileageCar);
+                    } else {
+                        System.out.println("No cars in the inventory.");
+                    }
+                } else if (option == 7) {
+                    // Display overall MPG for the entire inventory
+                    double averageMpg = carLot.getAverageMpg();
+                    System.out.println("Average MPG for the entire inventory: " + averageMpg);
+                } else if (option == 8) {
+                    // Display profit for all sold cars
+                    double totalProfit = carLot.getTotalProfit();
+                    System.out.println("Total profit for all sold cars: $" + totalProfit);
+                } else if (option == 9) {
+                    carLot.saveToDisk();
+                    System.out.println("Data Saved");
+                } else if (option == 10) {
+                    carLot.loadFromDisk();
+                    System.out.println("Data Loaded");
                 } else {
-                    System.out.println("No cars in the inventory.");
+                    System.out.println("Invalid option. Please enter a number from 0 to 10.");
                 }
-            } else if (option == 6) {
-                // Display car with the highest mileage
-                Car highestMileageCar = carLot.getCarWithHighestMileage();
-                if (highestMileageCar != null) {
-                    System.out.println("Car with the highest mileage:\n" + highestMileageCar);
-                } else {
-                    System.out.println("No cars in the inventory.");
-                }
-            } else if (option == 7) {
-                // Display overall MPG for the entire inventory
-                double averageMpg = carLot.getAverageMpg();
-                System.out.println("Average MPG for the entire inventory: " + averageMpg);
-            } else if (option == 8) {
-                // Display profit for all sold cars
-                double totalProfit = carLot.getTotalProfit();
-                System.out.println("Total profit for all sold cars: $" + totalProfit);
-            } else if (option == 9) {
-                carLot.saveToDisk();
-                System.out.println("Data Saved"); 
-            } else if (option == 10) {
-                carLot.loadFromDisk();
-                System.out.println("Data Loaded");
-            } else {
-                System.out.println("Invalid option. Please enter a number from 0 to 10.");
-            }
-        } catch (FileNotFoundException e) {
+            } catch (FileNotFoundException e) {
                 System.out.println("Error: File not found - " + e.getMessage());
-        }
-          catch (InputMismatchException e) {
+            }
+              catch (InputMismatchException e) {
             	System.out.println("Error: Invalid Input");
               }
             if (option != 0) {
@@ -138,4 +140,3 @@ public class CarLotMain {
             System.out.println(car);
         }
     }
-}
